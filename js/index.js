762f85ac17e81ad989eaba0d80470c7a7750d054
Jeramy Rutley <jrutley@gmail.com>
2017-01-21 01:19:33 -0500

    Restructured a bit. 100vh may come in handy

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
+function cutTo140Characters(text) {
+  if(text.length > 140){
+    return text.slice(0,137) + "...";
+  }
+  return text;
+}
+
 function retrieveQuote(){
   $.ajax({
     method: "GET",
-    url: "http://api.icndb.com/jokes/random",
+    url: "https://api.icndb.com/jokes/random",
     dataType: "json",
     success: function (data) {
-    $("#quote").html(data.value.joke);
+      var twitterLink = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(cutTo140Characters(data.value.joke));
+      $("#tweetlink").attr("href", twitterLink);
+      $("#quote").html(data.value.joke);
     },
     error: function(err){
       console.log(err)
